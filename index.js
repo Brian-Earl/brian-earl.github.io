82994f3a974d0d5cb6b47e98921ec75ed39d3d4e
Brian <brik71@gmail.com>
2022-03-24 15:24:14 -0400

    Site Version 3, Site Generator
    Complete rewrite of the site so that the site is generated using a json file using generated.html as the template.
    
    While the previous version of the site also used a json file, now instead of reading a hosted version of the json file and generating everything live, a  static site is generated with the file. This is a major improvement as it makes development easier. allows for faster loading, allows better customization per user, prevents downages if the json file cannot be read, and makes it whole system user friendly so others can actually use it and then customize their page without me there.
    
    Go to generate.html, input the json and press "generate page". This will send you to a preview of the site, then press "download here" on the preview page to download.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //Credit for better footers goes to https://stackoverflow.com/questions/53076202/stick-div-at-the-bottom-of-equally-height-cards
 //Credit for preventing doubles goes to https://stackoverflow.com/questions/1988349/array-push-if-does-not-exist
+//Credit for json parsing goes to https://gist.github.com/jokester/4a543ea76dbc5ae1bf05
+//Credit for save function goes to https://stackoverflow.com/questions/27177661/save-html-locally-with-javascript
+//Credit for shallow copy goes to https://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-deep-clone-an-object-in-javascript
 
-const projects = document.getElementById("projects");
 const aboutMe = document.getElementById("aboutMe");
 const aboutMeImage = document.getElementById("aboutMeImage");
-const filtersElement = document.getElementById("filters");
+const headerName = document.getElementById("headerName");
+const headerSocials = document.getElementById("headerSocials");
+const projectContainer = document.getElementById("projectContainer");
+const htmlTag = document.getElementById("html");
+const scriptTag = document.getElementById("script");
 
 const colors = new Map();
 colors.set("black", "dark");
@@ -16,24 +22,48 @@ colors.set("green", "success");
 colors.set("yellow", "warning");
 colors.set("red", "danger");
 
-let tagFilters = [];
+const socials = new Map();
+socials.set("email", "fas fa-envelope ")
+socials.set("github", "fab fa-github")
+socials.set("linkedin", "fab fa-linkedin-in")
+socials.set("pdf", "fas fa-file-alt")
 
-$.getJSON(
-  "https://raw.githubusercontent.com/Brian-Earl/brian-earl.github.io/master/index.json",
-  function (data) {
-    createAboutMe(data.aboutMe);
-    data.cards.forEach(createCard);
+function main() {
+  let hash = window.location.hash.slice(1);
+  let jsonStr = decodeURIComponent(hash)
+  let formattedData = JSON.parse(jsonStr);
+  createHeader(formattedData.header);
+  createAboutMe(formattedData.aboutMe);
+  formattedData.cardSections.forEach((element) => {
+    createCardSectionHeader(element.name);
+    if (formattedData.features.filters && formattedData.cardSections.length === 1) {
+      createFilterButtons(element.filters)
+
+      filtersElement = document.createElement("h4");
+      filtersElement.className = "row";
+      filtersElement.id = "filters";
+      projectContainer.appendChild(filtersElement);
+    }
+
+    projects = document.createElement("div");
+    projects.className = "row"
+    projectContainer.appendChild(projects);
+    element.cards.forEach((element) => { createCard(element, projects) });
+
+  })
+
+  if (formattedData.features.filters && formattedData.cardSections.length === 1) {
+    createFilterCode();
     addEvents();
   }
-);
+}
 
 function createAboutMe(aboutMeData) {
   aboutMe.innerHTML = aboutMeData.description;
-
   aboutMeImage.src = aboutMeData.image;
 }
 
-function createCard(cardData) {
+function createCard(cardData, projects) {
   base = document.createElement("div");
   base.className = "col-md-6 mb-5";
   projects.appendChild(base);
@@ -94,98 +124,190 @@ function createCard(cardData) {
   });
 }
 
-function addEvents() {
-  $(".tag-badge").on("click", function (e) {
-    addTagFilter(e.target.innerHTML);
+function createHeader(data) {
+  headerNameStrong = document.createElement("strong");
+  headerNameStrong.innerHTML = data.name;
+  headerName.appendChild(headerNameStrong);
+
+  data.socials.forEach(element => {
+    createHeaderSocialIcon(element);
   });
-
-  $(".add-filter-badge").on("click", function (e) {
-    clearTagFilters();
-    addTagFilter(e.target.attributes.filter.nodeValue);
-  });
 }
 
-function addTagFilter(filter) {
-  tagFilters.indexOf(filter) === -1
-    ? tagFilters.push(filter)
-    : console.log("This item already exists");
-  filterElements();
+function createHeaderSocialIcon(data) {
+  socialLink = document.createElement("a");
+  socialLink.style = "padding: 1em;"
+  socialLink.href = data.link;
+  headerSocials.appendChild(socialLink);
+
+  socialSpan = document.createElement("span");
+  socialSpan.className = "icon subtitle";
+  socialLink.appendChild(socialSpan);
+
+  socialIcon = document.createElement("i");
+  socialIcon.className = "text-white " + socials.get(data.type);
+  socialIcon.style = style = "font-size: 40px;"
+  socialSpan.appendChild(socialIcon);
 }
 
-function removeTagFilter(filter) {
-  tagFilters =
-    tagFilters.indexOf(filter) !== -1 &&
-    tagFilters.splice(tagFilters.indexOf(tagFilters), 1);
-  if (tagFilters.length) return;
-  tagFilters = [];
-  filterElements();
+function createCardSectionHeader(data) {
+  container = document.createElement("div");
+  container.className = "row";
+  projectContainer.appendChild(container);
+
+  cardSectionHeader = document.createElement("h1");
+  cardSectionHeader.innerHTML = data;
+  container.appendChild(cardSectionHeader);
 }
 
-function clearTagFilters() {
-  tagFilters = [];
-  filterElements();
+function createFilterButtons(data) {
+  container = document.createElement("h3");
+  container.className = "row";
+  container.id = "perminant-filters";
+  projectContainer.appendChild(container);
+
+  data.forEach((element) => {
+    filterButton = document.createElement("a");
+    filterButton.className = "badge badge-secondary add-filter-badge button-badge-spacing filter-button";
+    filterButton.setAttribute("filter", element.filter);
+    filterButton.innerHTML = element.display;
+    container.appendChild(filterButton);
+  })
 }
 
-function filterElements() {
-  filterRemovalBadges();
-  filterCards();
-  addFilterEvents();
+function save() {
+  copiedHTML = htmlTag.cloneNode(true)
+  removeElement(copiedHTML, "script", "mainScript")
+  removeElement(copiedHTML, "p", "previewView")
+  let htmlFile = new Blob([copiedHTML.outerHTML], { type: "text/html" });
+  let a = document.createElement("a");
+  a.href = URL.createObjectURL(htmlFile);
+  a.download = "generated.html";
+  a.hidden = true;
+  document.body.appendChild(a);
+  a.click();
 }
 
-function filterCards() {
-  let badgesElements = document.getElementsByClassName("badges");
-  Array.prototype.forEach.call(badgesElements, function (badges) {
-    let tags = [];
-    badges.childNodes.forEach((tag) => {
-      tags.push(tag.innerHTML);
+function removeElement(html, tag, id) {
+  let e = html.getElementsByTagName(tag).namedItem(id)
+  console.log(e)
+  e.parentElement.removeChild(e);
+}
+
+main()
+
+function createFilterCode() {
+  script = document.createElement("script");
+  //This is a hack horrible hack
+  //I am so so so so sorry to the unluck soul that is reading this
+  script.innerHTML = `
+  
+  let tagFilters = [];
+  const filtersElement = document.getElementById("filters");
+  addEvents();
+
+  function addEvents() {
+    $(".tag-badge").on("click", function (e) {
+      addTagFilter(e.target.innerHTML);
     });
-    let filterOut = false;
-    tagFilters.forEach((filter) => {
-      filterOut = filterOut || !tags.includes(filter);
+  
+    $(".add-filter-badge").on("click", function (e) {
+      clearTagFilters();
+      addTagFilter(e.target.attributes.filter.nodeValue);
     });
-    badges.parentNode.parentNode.parentNode.hidden = filterOut;
-  });
-}
-
-function filterRemovalBadges() {
-  removeAllChildNodes(filtersElement);
-  if (tagFilters.length <= 0) return;
-
-  if (tagFilters.length > 1) {
-    xIcon = document.createElement("i");
-    xIcon.className = "fas fa-times-circle text-white x-icon";
-    clearAllFilter = document.createElement("a");
-    clearAllFilter.className =
-      "badge badge-secondary clear-all-filter-badge button-badge-spacing filter-button";
-    clearAllFilter.innerHTML = "Remove All Filters";
-    clearAllFilter.append(xIcon);
-    filtersElement.append(clearAllFilter);
+  }
+  
+  function addTagFilter(filter) {
+    tagFilters.indexOf(filter) === -1
+      ? tagFilters.push(filter)
+      : console.log("This item already exists");
+    filterElements();
+  }
+  
+  function removeTagFilter(filter) {
+    tagFilters =
+      tagFilters.indexOf(filter) !== -1 &&
+      tagFilters.splice(tagFilters.indexOf(tagFilters), 1);
+    if (tagFilters.length) return;
+    tagFilters = [];
+    filterElements();
+  }
+  
+  function clearTagFilters() {
+    tagFilters = [];
+    filterElements();
+  }
+  
+  function filterElements() {
+    filterRemovalBadges();
+    filterCards();
+    addFilterEvents();
   }
 
-  tagFilters.forEach((element) => {
-    xIcon = document.createElement("i");
-    xIcon.className = "fas fa-times-circle text-white x-icon";
-    clearAllFilter = document.createElement("a");
-    clearAllFilter.className =
-      "badge badge-secondary clear-filter-badge button-badge-spacing filter-button";
-    clearAllFilter.innerHTML = element;
-    clearAllFilter.append(xIcon);
-    filtersElement.append(clearAllFilter);
-  });
-}
-
-function addFilterEvents() {
-  $(".clear-all-filter-badge").on("click", function () {
-    clearTagFilters();
-  });
-
-  $(".clear-filter-badge").on("click", function (e) {
-    removeTagFilter(e.target.innerHTML);
-  });
-}
-
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+  function filterElements() {
+    filterRemovalBadges();
+    filterCards();
+    addFilterEvents();
   }
+  
+  function filterCards() {
+    let badgesElements = document.getElementsByClassName("badges");
+    Array.prototype.forEach.call(badgesElements, function (badges) {
+      let tags = [];
+      badges.childNodes.forEach((tag) => {
+        tags.push(tag.innerHTML);
+      });
+      let filterOut = false;
+      tagFilters.forEach((filter) => {
+        filterOut = filterOut || !tags.includes(filter);
+      });
+      badges.parentNode.parentNode.parentNode.hidden = filterOut;
+    });
+  }
+  
+  function filterRemovalBadges() {
+    if(!filtersElement) return;
+    removeAllChildNodes(filtersElement);
+    if (tagFilters.length <= 0) return;
+  
+    if (tagFilters.length > 1) {
+      xIcon = document.createElement("i");
+      xIcon.className = "fas fa-times-circle text-white x-icon";
+      clearAllFilter = document.createElement("a");
+      clearAllFilter.className =
+        "badge badge-secondary clear-all-filter-badge button-badge-spacing filter-button";
+      clearAllFilter.innerHTML = "Remove All Filters";
+      clearAllFilter.append(xIcon);
+      filtersElement.append(clearAllFilter);
+    }
+  
+    tagFilters.forEach((element) => {
+      xIcon = document.createElement("i");
+      xIcon.className = "fas fa-times-circle text-white x-icon";
+      clearAllFilter = document.createElement("a");
+      clearAllFilter.className =
+        "badge badge-secondary clear-filter-badge button-badge-spacing filter-button";
+      clearAllFilter.innerHTML = element;
+      clearAllFilter.append(xIcon);
+      filtersElement.append(clearAllFilter);
+    });
+  }
+  
+  function addFilterEvents() {
+    $(".clear-all-filter-badge").on("click", function () {
+      clearTagFilters();
+    });
+  
+    $(".clear-filter-badge").on("click", function (e) {
+      removeTagFilter(e.target.innerHTML);
+    });
+  }
+  
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  `
+  htmlTag.appendChild(script);
 }
